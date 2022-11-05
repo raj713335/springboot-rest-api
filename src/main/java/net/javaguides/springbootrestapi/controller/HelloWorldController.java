d9438100c493bbd32ff69b583d0fc731ed31710d
Arnab Das <raj713335@gmail.com>
2022-11-06 00:11:27 +0530

    Initial commit

---
+++ src/main/java/net/javaguides/springbootrestapi/controller/HelloWorldController.java
@@ -0,0 +1,9 @@
+package net.javaguides.springbootrestapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+}
